4487ce8c98af0cf1ed78e4c42516512630e9e92b
dpizle <dpizle@yahoo.com>
2017-12-13 22:07:30 -0600

    WatchTest

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type" : "text/plain"});
-    response.end("<H1>Sup J-Dog, Does it work? </H1>  \n");
+    response.end("Sup J-Dog, Does it work?  \n");
 });
 
 server.listen(8080);
